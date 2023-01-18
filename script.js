ff711d4d9b700cf0894ac18c8a9bce486410fe24
Travis Schoniwitz <travisschoniwitz@gmail.com>
2023-01-17 21:31:07 -0600

    Code getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hello World!!");
+
+function getComputerChoice() {
+  let compChoice = Math.floor(Math.random() * 3) + 1;
+    if (compChoice === 1) {
+      return "Rock";
+      } else if (compChoice === 2) {
+      return "Paper";
+      } else {
+      return "Scissors";
+      }
+}
+
+
+console.log(getComputerChoice())

fc8a03ccb0e932177918c3916677e9898daa7ea4
Travis Schoniwitz <travisschoniwitz@gmail.com>
2023-01-25 00:28:56 -0600

    Complete step 4 and 5

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ console.log("Hello World!!");
 
 function getComputerChoice() {
   let compChoice = Math.floor(Math.random() * 3) + 1;
-    if (compChoice === 1) {
-      return "Rock";
-      } else if (compChoice === 2) {
-      return "Paper";
-      } else {
-      return "Scissors";
-      }
+  if (compChoice === 1) {
+    return "Rock";
+  } else if (compChoice === 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
 }
 
+function playRound(playerSelection, computerSelection) {
+  if ((playerSelection == "rock" && computerSelection == "Scissors") ||
+  (playerSelection == "paper" && computerSelection == "Rock") ||
+  (playerSelection == "scissors" && computerSelection == "Paper")) {
+    return "You Win!";
+  } else if ((playerSelection == "rock" && computerSelection == "Paper") ||
+    (playerSelection == "paper" && computerSelection == "Scissors") ||
+    (playerSelection == "scissors" && computerSelection == "Rock")) {
+    return "You Lose!";
+  } else {
+    return "Draw!";
+  } 
+}
 
-console.log(getComputerChoice())
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playerSelection, computerSelection);
+console.log(playRound(playerSelection, computerSelection));

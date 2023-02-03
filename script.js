41fe97170300320304705bec65707f02376af7e1
Travis Schoniwitz <travisschoniwitz@gmail.com>
2023-02-02 22:49:41 -0600

    Make result message more descriptive

--- script.js
+++ script.js
@@ -12,20 +12,39 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  if ((playerSelection == "rock" && computerSelection == "Scissors") ||
-  (playerSelection == "paper" && computerSelection == "Rock") ||
-  (playerSelection == "scissors" && computerSelection == "Paper")) {
-    return "You Win!";
-  } else if ((playerSelection == "rock" && computerSelection == "Paper") ||
-    (playerSelection == "paper" && computerSelection == "Scissors") ||
-    (playerSelection == "scissors" && computerSelection == "Rock")) {
-    return "You Lose!";
+  if ((playerSelection === "rock" && computerSelection === "Scissors") ||
+  (playerSelection === "paper" && computerSelection === "Rock") ||
+  (playerSelection === "scissors" && computerSelection === "Paper")) {
+    return result = `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else if ((playerSelection === "rock" && computerSelection === "Paper") ||
+    (playerSelection === "paper" && computerSelection === "Scissors") ||
+    (playerSelection === "scissors" && computerSelection === "Rock")) {
+    return result = `Computer Wins! ${computerSelection} beats ${playerSelection}`;
   } else {
-    return "Draw!";
+    return result = "Draw!";
   } 
 }
 
-const playerSelection = "rock";
+function game() {
+  playRound(playerSelection, computerSelection);
+  for (let i = 0; i < 5; i++) {
+    if (result === `You Win! ${playerSelection} beats ${computerSelection}`) {
+      console.log("You gained a point");
+    } else if (result === `Computer Wins! ${computerSelection} beats ${playerSelection}`) {
+      console.log("Computer gained a point");
+    } else {
+      console.log("No one gained a point");
+    }
+  }
+}
+
+
+const playerSelection = prompt("Enter rock/paper/scissors").toLowerCase();
 const computerSelection = getComputerChoice();
 console.log(playerSelection, computerSelection);
 console.log(playRound(playerSelection, computerSelection));
+console.log(game());
+
+/*
+
+*/
